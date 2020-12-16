bde4ff7668b95b4819499bb54489c4eb6a7c1a2d
JosueGabriel <josue_santos@edu.univali.br>
2020-12-16 19:25:41 -0300

    feat: upgrade sensor

--- src/collect/sensor.cpp
+++ src/collect/sensor.cpp
@@ -3,7 +3,18 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
+#include <random>
+#include <functional>
 //#include "reading.hpp"
+
+
+auto distribution(double mean, double sd) -> int {//std::function<double()>{
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+    std::normal_distribution<> d{mean, sd};
+    return d(gen);
+    //return [d, gen](){ return d(gen); }; 
+}
 
 auto main(int argc, char* argv[]) -> int {
     using namespace std::chrono_literals;
@@ -17,8 +28,8 @@ auto main(int argc, char* argv[]) -> int {
         std::cout << "Reading sensor...\n" << std::endl;
     
         auto id = std::string(argv[1]);
-        auto flow = 22;
-        auto status = "OK";
+        //auto flow = distribution(50, 10);
+        auto status = "OK"s;
          
         //  Prepare our context and publisher
         auto context = zmq::context_t(1);
@@ -26,14 +37,15 @@ auto main(int argc, char* argv[]) -> int {
         publisher.connect("tcp://localhost:5559");
 
         while (true) {
-            
-            auto msg = "ok";//"{\n id: "+id+",\n flow: "+flow+",\n status: "+status+" \n}";
+            auto flow = distribution(50, 10);
+
+            auto msg = "{\n id: "s+id+",\n flow: "+std::to_string(flow)+",\n status: "+status+" \n}";
 
             std::cout << msg << std::endl << std::endl;
             //  Send message to all subscribers
             publisher.send(zmq::buffer(msg), zmq::send_flags::none);
 
-            std::this_thread::sleep_for(400ms);
+            std::this_thread::sleep_for(1500ms);
         }
     }
 
